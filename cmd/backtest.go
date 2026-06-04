1ebf4eb05538ad9644bd24bb01e64b4fa7b13329
Hashir <70011762+HashMaster02@users.noreply.github.com>
2026-06-03 22:55:15 -0700

    Integrated basic strategy interface into the engine, and ensures mutex locks prevent concurrent access to event queue and data heap. Demo runs a very simple strategy against AAPL, for now

--- cmd/backtest.go
+++ cmd/backtest.go
@@ -4,22 +4,35 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/HashMaster02/slipstream/internal/data"
 	"github.com/HashMaster02/slipstream/internal/datastructures"
 	"github.com/HashMaster02/slipstream/internal/events"
+	"github.com/HashMaster02/slipstream/pkg/strategy"
+	"github.com/HashMaster02/slipstream/pkg/types"
 )
 
 type MarketDataPacket struct {
 	row data.Row
 	event events.Event
 }
-var rowHeap datastructures.RowHeap = datastructures.RowHeap{}  // This stores the data from the market data source
-var eventQueue datastructures.Queue = datastructures.NewQueue()
+
+type EngineState struct {
+	mu sync.Mutex
+	rowHeap datastructures.RowHeap
+	eventQueue datastructures.Queue
+}
+
+var engineState EngineState = EngineState{
+	rowHeap: datastructures.RowHeap{},
+	eventQueue: datastructures.NewQueue(),
+}
+
 
 func ReadData(reader *data.Reader, channel chan <- MarketDataPacket) {
-	for range 5 {
+	for {
 		data, err := reader.Next()
 		if err == io.EOF {
 			reader.CloseReader()
@@ -27,7 +40,7 @@ func ReadData(reader *data.Reader, channel chan <- MarketDataPacket) {
 		}
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
-			continue
+			break
 		}
 
 		e := events.MarketEvent{Type: events.MarketEventType, CreatedAt: time.Now()}
@@ -36,7 +49,7 @@ func ReadData(reader *data.Reader, channel chan <- MarketDataPacket) {
 }
 
 
-func ProcessChannels(c <- chan MarketDataPacket) {
+func ProcessChannels(c <- chan MarketDataPacket, engine *EngineState) {
 	// Both channels are unbuffered, which guarantees that we 
 	// push data onto the rowHeap BEFORE we push a corresponding
 	// MarketEvent onto the eventQueue (which we want). If we ever 
@@ -44,19 +57,24 @@ func ProcessChannels(c <- chan MarketDataPacket) {
 	for c != nil {
 		packet, ok := <- c
 		if !ok {c = nil; continue}
-		rowHeap.PushEntry(&packet.row)
-		eventQueue.Push(packet.event)
+
+		engine.mu.Lock()
+		engine.rowHeap.PushEntry(&packet.row)
+		engine.eventQueue.Push(packet.event)
+		engine.mu.Unlock()
 	}
 }
 
 
 func main() {
 
-	tickers := []string{"AAPL", "MSFT", "GS"}
+	tickers := []string{"AAPL"}
 
 	marketPacketChannel := make(chan MarketDataPacket)
 
-	go ProcessChannels(marketPacketChannel)
+	historicData := make(map[string][]*data.Row)
+
+	go ProcessChannels(marketPacketChannel, &engineState)
 
 	for _, ticker := range tickers {
 		filepath := fmt.Sprintf("./data/firstrate/stock_update_month_1min_adjsplit/%s_month_1min_adjsplit.txt", ticker)
@@ -66,35 +84,41 @@ func main() {
 			os.Exit(-1)
 		}
 
-		row, err := reader.Next()
-		if err == io.EOF {
-			reader.CloseReader()
-			continue
-		}
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-		rowHeap.PushEntry(&row)
 		go ReadData(reader, marketPacketChannel)
 	}
 
+
+	// Main engine loop. Perpetually runs until quit with Ctrl+C
+	// TODO: Shutdown engine gracefully on KILL signal (Ctrl+C)
+	strategy := strategy.NewBuyAndHoldOnClose(tickers, types.PriceFromFloat(269.00), types.PriceFromFloat(0.50), 100)
 	for {
-		event, succ := eventQueue.Pop()
+		// The engine waits for an event
+		engineState.mu.Lock()
+		event, succ := engineState.eventQueue.Pop()
+		engineState.mu.Unlock()
 		if !succ {
-			break
+			continue
 		}
 
 		switch e := event.(type) {
 			case events.MarketEvent: {
-				fmt.Printf("EVENT:: Type=%s, EngineTimestamp: %v\n", e.Type, e.CreatedAt)
-				row := rowHeap.PopEntry()
-				fmt.Printf("Symbol: %s, Timestamp: %v\n", row.Symbol, row.Timestamp)
-			}
+				
+				engineState.mu.Lock()
+				// ======= run in go routine for each active strategy ========
+				order, succ := strategy.CalculateSignals(&engineState.rowHeap, &historicData)
+				if succ {
+					engineState.eventQueue.Push(order)
+				}
+				// ===========================================================
 
+				row := engineState.rowHeap.PopEntry()
+				engineState.mu.Unlock()
+				historicData[row.Symbol] = append(historicData[row.Symbol], row)
+			}
+			case events.OrderEvent: {
+				fmt.Printf("BarTimestamp: %v, OrderType: %s, Price: %s, Symbol: %s\n", e.BarTimestamp, e.OrderType.String(), e.Price.String(), e.Symbol)
+			}
 		}
 	}
-
-	close(marketPacketChannel)
 	
 }
