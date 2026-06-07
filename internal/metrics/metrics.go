eadde9c80a868e1fde5f8f240fedd14a410f1615
Hashir <70011762+HashMaster02@users.noreply.github.com>
2026-06-06 21:04:04 -0700

    Created a new 'metrics' package that will have functions to compute various performance metrics. Has some very simple functions for now

---
+++ internal/metrics/metrics.go
@@ -0,0 +1,42 @@
+package metrics
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/HashMaster02/slipstream/internal/portfolio"
+	"github.com/HashMaster02/slipstream/pkg/types"
+)
+
+func NetAssetValue(portfolio *portfolio.Portfolio) string {
+	var perfLog strings.Builder
+
+	for ticker, pos := range portfolio.Positions {
+		if pos.Qty != 0 {
+			fmt.Fprintf(&perfLog, "%s: %d shares at $%s/share\n", ticker, pos.Qty, pos.SharePrice.String())
+		}
+	}
+
+	return perfLog.String()
+}
+
+func LargestPosition(portfolio *portfolio.Portfolio) (string, types.Price) {
+	type MaxPosition struct {
+		MarketValue types.Price
+		Symbol		string
+	}
+
+	var max MaxPosition = MaxPosition{MarketValue: 0, Symbol: ""}
+
+	for ticker, pos := range portfolio.Positions {
+		if pos.Qty != 0 {
+			marketVal := pos.SharePrice * types.Price(pos.Qty)
+			if max.MarketValue <  marketVal {
+				max.MarketValue = marketVal
+				max.Symbol = ticker
+			}
+		}
+	}
+
+	return max.Symbol, max.MarketValue
+}
